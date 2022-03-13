a417776817926bd72720f127c424e51d547f2193
Apri Pandu <sarahpalastrin@gmail.com>
2022-03-14 03:09:04 +0700

    Update `crouch` Snippets

--- crouch/crouch.lua
+++ crouch/crouch.lua
@@ -40,35 +40,40 @@ function WalkCommandStart(source, args, raw)
 end
 
 Citizen.CreateThread( function()
-    while true do 
-        Citizen.Wait(0)
-        local ped = PlayerPedId()
-        if (DoesEntityExist(ped) and not IsEntityDead(ped)) then 
-            DisableControlAction( 0, 36, true ) -- INPUT_DUCK 
-            if ( not IsPauseMenuActive() ) then 
-                if (IsDisabledControlJustPressed(0, 36)) then 
-                    RequestAnimSet("move_ped_crouched")
-                    while (not HasAnimSetLoaded("move_ped_crouched")) do 
-                        Citizen.Wait(100)
-                    end
-                    if (crouched == true) then 
-                        if curentWalkCok then 
-                            ResetPedMovementClipset(ped, 0)
-                            RequestAnimSet(curentWalkCok)
-                            while not HasAnimSetLoaded(curentWalkCok) do
-                                Citizen.Wait(100)
-                            end
-                            SetPedMovementClipset(ped, curentWalkCok, 0.2)
-                        else
-                            ResetPedMovementClipset(ped, 0)
-                        end
-                        crouched = false 
-                    elseif (crouched == false) then
-                        SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
-                        crouched = true 
-                    end 
-                end
-            end 
+  while true do 
+    Citizen.Wait(0)
+    local ped = PlayerPedId()
+
+    if (DoesEntityExist(ped) and not IsEntityDead(ped)) then 
+      DisableControlAction( 0, 36, true ) -- INPUT_DUCK 
+      if ( not IsPauseMenuActive() ) then 
+        if (IsDisabledControlJustPressed(0, 36)) then 
+          RequestAnimSet("move_ped_crouched")
+          while (not HasAnimSetLoaded("move_ped_crouched")) do 
+            Citizen.Wait(100)
+          end
+
+          if (crouched == true) then 
+            if curentWalkCok then 
+              ResetPedMovementClipset(ped, 0)
+              RequestAnimSet(curentWalkCok)
+
+              while not HasAnimSetLoaded(curentWalkCok) do
+                Citizen.Wait(100)
+              end
+
+              SetPedMovementClipset(ped, curentWalkCok, 0.2)
+            else
+              ResetPedMovementClipset(ped, 0)
+            end
+
+            crouched = false 
+          elseif (crouched == false) then
+            SetPedMovementClipset(ped, "move_ped_crouched", 0.25)
+              crouched = true 
+          end 
         end
+      end 
     end
+  end
 end)
