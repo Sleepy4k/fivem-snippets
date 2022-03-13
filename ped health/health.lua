8609a5038255d4fb7d71f7833a56f1ce018395c5
Apri Pandu <sarahpalastrin@gmail.com>
2022-03-14 01:59:52 +0700

    Update `ped health` Snippets

--- ped health/health.lua
+++
@@ -1,7 +0,0 @@
-AddEventHandler('playerSpawned', function(spawn)
-    local ped = PlayerPedId()
-    if GetPedMaxHealth(ped) ~= 200 and not IsEntityDead(ped) then
-        SetPedMaxHealth(ped, 200)
-        SetEntityHealth(ped, GetEntityHealth(ped) + 25)
-    end
-end)
