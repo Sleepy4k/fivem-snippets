0c4af36d62e1e36f17a81991cf8659fb03634d13
Apri Pandu <sarahpalastrin@gmail.com>
2022-03-14 03:06:25 +0700

    Update `helmet` Snippets

--- helmet/disable_auto_helmet.lua
+++
@@ -1,8 +0,0 @@
-Citizen.CreateThread( function()
-    while true do
-      Citizen.Wait(0)        --Edit this if the resource is using too much resources.   
-      local p = GetPlayerPed(-1)
-      local pV = GetVehiclePedIsUsing(p)
-      if pV ~= 0 then SetPedConfigFlag(p, 35, false) end
-    end    
-end)
