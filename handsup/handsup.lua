960e1478014f769c3068d6fb94ea2554161a9d32
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:52:43 +0700

    Change handsup method

--- handsup/handsup.lua
+++ handsup/handsup.lua
@@ -1,21 +1,28 @@
+--// don't touch this please \\--
+local taskplayanim = TaskPlayAnim
+local doesentityexist = DoesEntityExist
+local requestanimdict = RequestAnimDict
+local hasanimdictloaded = HasAnimDictLoaded
+local ClearPedSecondaryTask = ClearPedSecondaryTask
+
 RegisterKeyMapping('+handsup', 'Angkat Tangan', 'keyboard', 'x')
 
 RegisterCommand('+handsup', function()
 	local ped = PlayerPedId()
 
-	if DoesEntityExist(ped) then 
+	if doesentityexist(ped) then 
 		CreateThread(function()  
-			RequestAnimDict("random@mugging3")
-			while not HasAnimDictLoaded("random@mugging3") do 
+			requestanimdict("random@mugging3")
+			while not hasanimdictloaded("random@mugging3") do 
 				Wait(100) 
 			end
 
-			TaskPlayAnim(ped, "random@mugging3", "handsup_standing_base", 8.0, -8, -1, 49, 0, 0, 0, 0)
+			taskplayanim(ped, "random@mugging3", "handsup_standing_base", 8.0, -8, -1, 49, 0, 0, 0, 0)
 		end)
 	end
 end)
 
 RegisterCommand('-handsup', function() 
 	local ped = PlayerPedId()
-  	ClearPedSecondaryTask(ped)
+	clearpedsecondarytask(ped)
 end)
