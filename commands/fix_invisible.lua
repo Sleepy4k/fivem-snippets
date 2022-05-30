8e77cb49f809be8c4777f6aea2b9e0784ad21682
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:51:21 +0700

    Change fix invisible method

--- commands/fix_invisible.lua
+++ commands/fix_invisible.lua
@@ -1,9 +1,16 @@
+--// don't touch this please \\--
+local setentityvisible = SetEntityVisible
+local setplayervisiblelocally = SetPlayerVisibleLocally
+local setentitylocallyvisible = SetEntityLocallyVisible
+local setlocalplayervisiblelocally = SetLocalPlayerVisibleLocally
+local networksetentityinvisibletonetwork = NetworkSetEntityInvisibleToNetwork
+
 RegisterCommand('fixinvis', function(source, args, RawCommand)
     local ped = PlayerPedId()
         
-    SetEntityLocallyVisible(ped)
-    NetworkSetEntityInvisibleToNetwork(ped, false)
-    SetPlayerVisibleLocally(source, true)
-    SetEntityVisible(ped, true, 0)
-    SetLocalPlayerVisibleLocally(true)
+    setentitylocallyvisible(ped)
+    networksetentityinvisibletonetwork(ped, false)
+    setplayervisiblelocally(source, true)
+    setentityvisible(ped, true, 0)
+    setlocalplayervisiblelocally(true)
 end)
