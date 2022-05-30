3add1600b38e46fac4d68371473883ad0f4184fd
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 08:33:21 +0700

    Update and optimize anti spectate

--- anticheat/anti_spectate.lua
+++ anticheat/anti_spectate.lua
@@ -1,9 +1,18 @@
-Citizen.CreateThread(function()
-  while true do
-    if _NetworkIsInSpectatorMode() then
-      --Trigger ke serverside mu untuk drop player dan log ke  discord
-      Wait(1*60000)
-    end
-    Wait(2000)
-  end
+local enable_anti_spectate = true
+
+--// don't touch this please \\--
+local networkisinspectatormode = NetworkIsInSpectatorMode
+
+CreateThread(function()
+	while true do
+		local msec = 2000
+
+		if networkisinspectatormode() and enable_anti_spectate then
+			--// use your server side trigger for kick player and discord log \\--
+			--// gunakan trigger server side kalian untuk kick player dan log discord \\-- 
+			msec = 1*60000
+		end
+
+		Wait(msec)
+	end
 end)
