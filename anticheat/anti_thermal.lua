ef011c2c562dc0fd92b4f9335a3b5e44dabbb16b
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 08:33:32 +0700

    Update and optimize anti thermal

--- anticheat/anti_thermal.lua
+++ anticheat/anti_thermal.lua
@@ -1,5 +1,22 @@
-if GetUsingseethrough() then
-    if not IsPedInAnyVehicle(PlayerPedId(), false) then
-        -- trigger ke serverside biar ke ke kick
+local enbale_anti_thermal = true
+
+--// don't touch this please \\--
+local ispedonfoot = IsPedOnFoot
+local getusingseethrough = GetUsingseethrough
+
+CreateThread(function()
+    while true do
+        local msec = 500
+        local ped = PlayerPedId()
+
+        if ispedonfoot(ped) and enbale_anti_thermal then
+            if getusingseethrough() then
+                msec = 0
+                --// use your server side trigger for kick player and discord log \\--
+				--// gunakan trigger server side kalian untuk kick player dan log discord \\-- 
+            end
+        end
+
+        Wait(msec)
     end
-end
+end)
