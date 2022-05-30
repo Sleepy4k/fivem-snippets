6b272aa1ea9a2145cc99de44b2c67200f850433e
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:55:31 +0700

    Change density method

--- npc/density.lua
+++ npc/density.lua
@@ -1,20 +1,35 @@
+--// don't touch this please \\--
+local setrandomboats = SetRandomBoats
+local setrandomtrains = SetRandomTrains
+local setgarbagetrucks = SetGarbageTrucks
+local setcreaterandomcops = SetCreateRandomCops
+local distantcopcarsirens = DistantCopCarSirens
+local enabledispatchservice = EnableDispatchService
+local setpedpopulationbudget = SetPedPopulationBudget
+local setdispatchcopsforplayer = SetDispatchCopsForPlayer
+local setnumberofparkedvehicles = SetNumberOfParkedVehicles
+local setvehiclepopulationbudget = SetVehiclePopulationBudget
+local disablevehicledistantlights = DisableVehicleDistantlights
+local setcreaterandomcopsonscenarios = SetCreateRandomCopsOnScenarios
+local setcreaterandomcopsnotonscenarios = SetCreateRandomCopsNotOnScenarios
+
 CreateThread(function()
     local id = PlayerId()
 
     for i = 1, 15 do 
-        EnableDispatchService(i, false) 
+        enabledispatchservice(i, false) 
     end
 
-    SetRandomBoats(false)
-    SetGarbageTrucks(false)
-    SetRandomTrains(false)
-    SetCreateRandomCops(false)
-    SetCreateRandomCopsOnScenarios(false)
-    SetCreateRandomCopsNotOnScenarios(false)
-    SetDispatchCopsForPlayer(id, false)
-    SetPedPopulationBudget(0.0)
-    SetNumberOfParkedVehicles(0.0)
-    SetVehiclePopulationBudget(0.0)
-    DistantCopCarSirens(false)
-    DisableVehicleDistantlights(true)
+    setrandomboats(false)
+    setgarbagetrucks(false)
+    setrandomtrains(false)
+    setcreaterandomcops(false)
+    setcreaterandomcopsonscenarios(false)
+    setcreaterandomcopsnotonscenarios(false)
+    setdispatchcopsforplayer(id, false)
+    setpedpopulationbudget(0.0)
+    setnumberofparkedvehicles(0.0)
+    setvehiclepopulationbudget(0.0)
+    distantcopcarsirens(false)
+    disablevehicledistantlights(true)
 end)
