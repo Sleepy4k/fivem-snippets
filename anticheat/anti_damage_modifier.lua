8119cf15e1c9a6d7601be02ab5071383d8ad6c0a
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 08:32:52 +0700

    Update and optimize anti damage modifier

--- anticheat/anti_damage_modifier.lua
+++ anticheat/anti_damage_modifier.lua
@@ -1,6 +1,28 @@
-local _, weapon = GetCurrentPedWeapon(PlayerPedId(), 1)
+local enable_anti_modifier = true
 
-if _GetWeaponDamageModifier(weapon) > 1.0 then -- bisa juga set ke damage tertinggi di dalam server. misal paling tinggi 0.56, berarti damage > 0.57 then ..
-  --Trigger ke serverside mu untuk drop player dan log ke  discord
-  Wait(2000)
-end
+--// don't touch this please \\--
+local ispedarmed = IsPedArmed
+local getcurrentpedweapon = GetCurrentPedWeapon
+local getweapondamagemodifier = GetWeaponDamageModifier
+
+CreateThread(function()
+	while true do
+		local msec = 500
+		local ped = PlayerPedId()
+
+		if ispedarmed(ped, 4) and enable_anti_modifier then
+			msec = 0
+			local _, weapon = getcurrentpedweapon(ped, 1)
+
+			--// Change value if you feel too many triggering \\--
+			--// Ganti angka validasi jika kamu merasa terlalu banyak melakukan trigger \\--
+			if getweapondamagemodifier(weapon) > 1.0 then
+				--// use your server side trigger for kick player and discord log \\--
+				--// gunakan trigger server side kalian untuk kick player dan log discord \\-- 
+				Wait(2000)
+			end
+		end
+
+		Wait(msec)
+	end
+end)
