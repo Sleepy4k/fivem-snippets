7b57414f6b6f1c3467c7763b5afc236e10f418b5
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:51:31 +0700

    Change fix property method

--- commands/fix_property.lua
+++ commands/fix_property.lua
@@ -1,15 +1,23 @@
 local notif = false
 
+--// don't touch this please \\--
+local getgamepool = GetGamePool
+local detachentity = DetachEntity
+local deleteobject = DeleteObject
+local deleteentity = DeleteEntity
+local setentityasmissionentity = SetEntityAsMissionEntity
+local isentityattachedtoentity = IsEntityAttachedToEntity
+
 RegisterCommand('fixprop', function(source, args, RawCommand)
     local ped = PlayerPedId()
-    local game_pool = GetGamePool('CObject')
+    local game_pool = getgamepool('CObject')
 
     for k,v in pairs(game_pool) do
-        if IsEntityAttachedToEntity(ped, v) then
-            SetEntityAsMissionEntity(v, true, true)
-            DetachEntity(v, true, false)
-            DeleteObject(v)
-            DeleteEntity(v)
+        if isentityattachedtoentity(ped, v) then
+            setentityasmissionentity(v, true, true)
+            detachentity(v, true, false)
+            deleteobject(v)
+            deleteentity(v)
             notif = true
         end
     end
