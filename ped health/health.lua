6673be16c96fb7da502da973c8c6a6fd49e2f700
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:55:44 +0700

    Change health method

--- ped health/health.lua
+++ ped health/health.lua
@@ -1,13 +1,21 @@
+--// don't touch this please \\--
+local isentitydead = IsEntityDead
+local getpedmaxhealth = GetPedMaxHealth
+local setpedmaxhealth = SetPedMaxHealth
+local getentityhealth = GetEntityHealth
+local setentityhealth = SetEntityHealth
+local getentitymaxhealth = GetEntityMaxHealth
+
 AddEventHandler('playerSpawned', function(spawn)
     local ped = PlayerPedId()
 
-    if GetPedMaxHealth(ped) ~= 200 and not IsEntityDead(ped) then
-        SetPedMaxHealth(ped, 200)
+    if getpedmaxhealth(ped) ~= 200 and not isentitydead(ped) then
+        setpedmaxhealth(ped, 200)
 
-        local max_health = GetEntityMaxHealth(ped)
-        local ped_health = GetEntityHealth(ped)
+        local max_health = getentitymaxhealth(ped)
+        local ped_health = getentityhealth(ped)
         local health_boost = max_health - ped_health
 
-        SetEntityHealth(ped, ped_health + health_boost)
+        setentityhealth(ped, ped_health + health_boost)
     end
 end)
