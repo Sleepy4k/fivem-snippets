6c210c5534de09eef457cd0ad1553c993e2143e0
Apri Pandu <sarahpalastrin@gmail.com>
2022-05-30 09:52:05 +0700

    Change driveby method

--- drive by/driveby.lua
+++ drive by/driveby.lua
@@ -1,20 +1,25 @@
+--// don't touch this please \\--
+local getvehiclepedisin = GetVehiclePedIsIn
+local getpedinvehicleseat = GetPedInVehicleSeat
+local setplayercandodriveby = SetPlayerCanDoDriveBy
+
 CreateThread(function()
 	while true do
 		local msec = 500
 
 		local ped = PlayerPedId()
-		local ped_in_vehicle = GetVehiclePedIsIn(ped, false)
+		local ped_in_vehicle = getvehiclepedisin(ped, false)
 
 		if ped_in_vehicle then
 			msec = 0
             local id = PlayerId()
 
-			if GetPedInVehicleSeat(ped_in_vehicle, -1) == ped then
-				SetPlayerCanDoDriveBy(id, false)
+			if getpedinvehicleseat(ped_in_vehicle, -1) == ped then
+				setplayercandodriveby(id, false)
 			elseif passengerDriveBy then
-				SetPlayerCanDoDriveBy(id, true)
+				setplayercandodriveby(id, true)
 			else
-				SetPlayerCanDoDriveBy(id, false)
+				setplayercandodriveby(id, false)
 			end
 		end
 
